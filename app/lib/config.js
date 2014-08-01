153973d5907f723de56188bca7ed43c9663f2d36
Mikey Badr <mfbadr@gmail.com>
2014-08-01 12:50:36 -0500

    added faq contact about

---
+++ app/lib/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(app){
+
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/../views');
+  
+  console.log('Express is configured...');
+};
+
---
+++ app/lib/pipeline.js
@@ -0,0 +1,21 @@
+'use strict';
+//var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var home = require('../controllers/home');
+
+module.exports = function(app, express){
+  app.use(morgan('dev'));
+  app.use(express.static(__dirname + '/../static'));
+  app.use(bodyParser.urlencoded({extended:true}));
+
+  app.get('/', home.index);
+
+  app.get('/about', home.about);
+  app.get('/faq', home.faq);
+  app.get('/contact', home.contact);
+
+
+  console.log('pipeline is GO!');
+};
+
