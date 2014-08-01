473ec7c126a6e69558b5a106651e90bf8a8f23bb
Mikey Badr <mfbadr@gmail.com>
2014-08-01 12:03:14 -0500

    refactoring

--- app/index.js
+++ app/index.js
@@ -1,25 +1,28 @@
 'use strict';
 
 var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
+//var morgan = require('morgan');
+//var bodyParser = require('body-parser');
+// ^^^^^^ thse are only used in our pipeline, so we dont need them here
+
 var app = express();
 
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
+var config = require('./lib/config');
+config(app);
+//could also use require('.lib/config')(app);
+// this is extra ninja
 
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/static'));
+// lets import and call our pipeline the ninja way!
+//var pipeline = require('./lib/pipeline')
+//pipeline(app, express);
+require('./lib/pipeline')(app, express);
 
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/', function (req, res){
-  res.render('home');
-})
+// declare db name when initializing, just like PORT
+var db = process.env.DB;
+require('./lib/mongodb')(db);
 
 var port = process.env.PORT;
-
 app.listen(port, function(){
   console.log('Express is listening on PORT', port + '...');
 });
